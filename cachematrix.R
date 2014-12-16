5e9eab84293a982d7a47313c9f4a937d129abb7f
Jouni Lehti <jouni.lehti@iki.fi>
2014-12-16 08:24:56 +0000

    first version of the cache matrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## JL 16.12.2014 Coursera R Programming Assignment 2
+## Optimization of inverting matrixes
 
-## Write a short comment describing this function
+## makeCacheMatric to create a vector for maintenance functions:
+## 1. set the value of the vector
+## 2. get the value of the vector
+## 3. set the value of the mean
+## 4. get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve to check if the inverted matrix already exists in the vector, if not, then solve() it to create the inversion
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$getmatrix()
+        if(!is.null(m)) { # check the cached vector
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...) # calculate the inversion
+        x$setmatrix(m)
+        m
 }
